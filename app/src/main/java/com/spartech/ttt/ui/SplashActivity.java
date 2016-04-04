1ed0e4b2ae02f66725a18a1e1fc34582fa63d445
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-05 00:26:03 +0100

    Add a splash activity.

---
+++ app/src/main/java/com/spartech/ttt/ui/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.spartech.ttt.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
