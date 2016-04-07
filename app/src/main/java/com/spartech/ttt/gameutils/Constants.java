44a047b6ef2c09a151122b1a487e0b1a0fd04b88
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-07 20:32:50 +0100

    Connect to distant server.

--- app/src/main/java/com/spartech/ttt/gameutils/Constants.java
+++ app/src/main/java/com/spartech/ttt/gameutils/Constants.java
@@ -4,8 +4,10 @@ package com.spartech.ttt.gameutils;
  * Created by Mohammed Aouf ZOUAG on 04/04/2016.
  */
 public class Constants {
+    // For debugging
     public static final String SERVER_IP = "192.168.173.1";
     public static final String SERVER_PORT = "5000";
-    public static final String SERVER_IP_ADDRESS =
-            String.format("http://%s:%s", SERVER_IP, SERVER_PORT);
+
+    // Production
+    public static final String SERVER_IP_ADDRESS = "https://ticsockettoe.herokuapp.com";
 }
